a2bef642a718389658a4933a5d7c56cc8b5f7ed7
zhangbin <zhangbincodec@gmail.com>
2016-10-21 11:17:57 +0800

    refer to https://dpsm.wordpress.com/2012/07/28/android-mediacodec-decoded/ to make the process more clear ,

--- extractor_decoder/app/src/main/java/io/vec/demo/mediacodec/DecodeActivity.java
+++ extractor_decoder/app/src/main/java/io/vec/demo/mediacodec/DecodeActivity.java
@@ -13,10 +13,14 @@ import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
+/*
+*  20161021 refer to https://dpsm.wordpress.com/2012/07/28/android-mediacodec-decoded/ to make the process more clear ,
+*  furthermore , plan to add audio decoder .
+* */
 public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 	private static final String SAMPLE = Environment.getExternalStorageDirectory() + "/libCGE/video.mp4";
 	private PlayerThread mPlayer = null;
+	private String T="ZB";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +57,7 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 	private class PlayerThread extends Thread {
 		private MediaExtractor extractor;
 		private MediaCodec decoder;
+		private MediaCodec audioDecoder;
 		private Surface surface;
 
 		public PlayerThread(Surface surface) {
@@ -63,15 +68,22 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 		public void run() {
 			extractor = new MediaExtractor();
 			extractor.setDataSource(SAMPLE);
-
+			Log.d(T,String.format("TRACKS #: %d",extractor.getTrackCount()));
 			for (int i = 0; i < extractor.getTrackCount(); i++) {
 				MediaFormat format = extractor.getTrackFormat(i);
 				String mime = format.getString(MediaFormat.KEY_MIME);
-				if (mime.startsWith("video/")) {
+				Log.d(T, String.format("#%d#MIME TYPE: %s",i, mime));
+				if (mime.startsWith("video/")) {/* video/avc*/
 					extractor.selectTrack(i);
 					decoder = MediaCodec.createDecoderByType(mime);
 					decoder.configure(format, surface, null, 0);
-					break;
+				//	break;
+				}
+				if(mime.startsWith("audio/")){ /*audio/mp4a-latm*/
+					Log.d(T, String.format("MIME TYPE: %s", mime));
+					extractor.selectTrack(i);
+					audioDecoder=MediaCodec.createDecoderByType(mime);
+					audioDecoder.configure(format,null/*surface*/,null/*croypto*/,0/*flag*/);
 				}
 			}
 
@@ -79,51 +91,69 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 				Log.e("DecodeActivity", "Can't find video info!");
 				return;
 			}
-
+			if(audioDecoder==null){
+				Log.e(T,"AUDIO DECODER CREATED FAIL");
+				return;
+			}
 			decoder.start();
+			audioDecoder.start();
 			/*每一个实例都有一个输入和一个输出缓冲*/
 			ByteBuffer[] inputBuffers = decoder.getInputBuffers();
 			ByteBuffer[] outputBuffers = decoder.getOutputBuffers();
 			BufferInfo info = new BufferInfo();
-			boolean isEOS = false;
+			boolean sawInputEOS = false;//isEOS
+			long presentationTimeUs = 0;
 			long startMs = System.currentTimeMillis();
 /*对于decoder，input buffer里是编码好的数据，对于encoder，output buffer里编码好的数据*/
 			while (!Thread.interrupted()) {
-				if (!isEOS) {
+				if (!sawInputEOS) {
+					/*====================================decoder的输入端处理 begin=====================================*/
 					/*当实例有可用的数据的时候，要把数据传递给codec以解码，调用下面的接口实现实例把input buffer的数据丢给codec*/
 					int inIndex = decoder.dequeueInputBuffer(10000);
 					if (inIndex >= 0) {
-						ByteBuffer buffer = inputBuffers[inIndex];
-						int sampleSize = extractor.readSampleData(buffer, 0);
+						ByteBuffer dstBuf = inputBuffers[inIndex];/*实例拿codec的未用的输入缓冲*/
+						int sampleSize = extractor.readSampleData(dstBuf, 0);/*让extractor放入demux出来的数据*/
 						if (sampleSize < 0) {
+							sawInputEOS = true;/*extractor拿不到数据就认为EOS了*/
+							sampleSize=0;//make it 0 to tell eos
+							presentationTimeUs=0;
 							// We shouldn't stop the playback at this point, just pass the EOS
 							// flag to decoder, we will get it again from the
 							// dequeueOutputBuffer
 							Log.d("DecodeActivity", "InputBuffer BUFFER_FLAG_END_OF_STREAM");
-							/*文档说是通过以下接口，发送带有BUFFER_FLAG_END_OF_STREAM 下标的信号，表示已经EOS了*/
-							decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
-							isEOS = true;
+/*							*//*文档说是通过以下接口，发送带有BUFFER_FLAG_END_OF_STREAM 下标的信号，表示已经EOS了*//*
+							decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);*/
+
 						} else {
-							decoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
-							extractor.advance();
+							presentationTimeUs = extractor.getSampleTime();
 						}
+						/*跟上面EOS的信号合并为一行代码了*/
+						/*填充好未解码数据的input buffer 丢给codec处理，数据大小及时间戳都送入*/
+						decoder.queueInputBuffer(inIndex, 0/*offset*/, sampleSize,presentationTimeUs ,sawInputEOS ? MediaCodec.BUFFER_FLAG_END_OF_STREAM: 0);
+						if(!sawInputEOS)/*extractor能拿到数据才继续取数据*/
+							extractor.advance();
 					}
-				}
+				}//end of if(!sawInputEOS)
+     			/*====================================decoder的输入端处理 end=====================================*/
+				/*====================================decoder从codec的输出端拉取解码后数据的处理过程 begin=====================================*/
 				/*从codec拿outputbuffer到实例*//*这是通过解码器的输出的数据的信息判断是不是EOS了*/
-				int outIndex = decoder.dequeueOutputBuffer(info, 10000);
-				switch (outIndex) {
+				final int res = decoder.dequeueOutputBuffer(info, 10000);
+				switch (res) {
 				case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
 					Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
 					outputBuffers = decoder.getOutputBuffers();
 					break;
 				case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+					final MediaFormat oformat = decoder.getOutputFormat();
+					Log.d(T, "Output format has changed to " + oformat);
 					Log.d("DecodeActivity", "New format " + decoder.getOutputFormat());
 					break;
 				case MediaCodec.INFO_TRY_AGAIN_LATER:
 					Log.d("DecodeActivity", "dequeueOutputBuffer timed out!");
 					break;
 				default:
-					ByteBuffer buffer = outputBuffers[outIndex];
+					int outputBufIndex = res;
+					ByteBuffer buffer = outputBuffers[outputBufIndex];
 					Log.v("DecodeActivity", "We can't use this buffer but render it due to the API limit, " + buffer);
 
 					// We use a very simple clock to keep the video FPS, or the video
@@ -138,7 +168,7 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 					}
 					/*实例调用下面接口会把这个outputbuffer还给codec，如果还设置了surface，那么outputbuffer的内容还会显示出来*/
 					// 将解码后数据渲染到surface上
-					decoder.releaseOutputBuffer(outIndex, true);
+					decoder.releaseOutputBuffer(outputBufIndex, true/*render*/);
 					break;
 				}
 
@@ -150,6 +180,8 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
 			}
 
 			decoder.stop();
+			audioDecoder.stop();
+			audioDecoder.release();
 			decoder.release();
 			extractor.release();
 		}
